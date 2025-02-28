88fa476e7c9415733215c4780a749fe14d5e582d
haran2231 <haran2231@gmail.com>
2025-02-28 15:06:28 +0530

    nine

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./Component/About";
 import Banner from "./Component/Banner";
 import BlogSection from "./Component/BlogSection";
@@ -7,19 +8,34 @@ import Footer from "./Component/Footer";
 import Header from "./Component/Header";
 import Services from "./Component/Services";
 import StatsSection from "./Component/StatsSection";
+import SingleBlog from "./Component/SingleBlog";
 
 function App() {
   return (
-    <>
-      <Header />
-      <section id="banner"><Banner /></section>
-      <section id="about"><AboutUs /></section>
-      <section id="services"><Services /></section>
-      <section id="stats"><StatsSection /></section>
-      <section id="blog"><BlogSection /></section>
-      <section id="contact"><ContactForm /></section>
-      <Footer />
-    </>
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          {/* Home Page with Sections */}
+          <Route
+            path="/"
+            element={
+              <>
+                <section id="banner"><Banner /></section>
+                <section id="about"><AboutUs /></section>
+                <section id="services"><Services /></section>
+                <section id="stats"><StatsSection /></section>
+                <section id="blog"><BlogSection /></section>
+                <section id="contact"><ContactForm /></section>
+              </>
+            }
+          />
+          {/* Single Blog Page */}
+          <Route path="/blog/:id" element={<SingleBlog />} />
+        </Routes>
+        <Footer />
+      </>
+    </Router>
   );
 }
 

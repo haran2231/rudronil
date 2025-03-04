cfb966b9a906445829df8550856763ef0bdc7b8d
haran2231 <haran2231@gmail.com>
2025-03-04 18:28:57 +0530

    final 1.5

--- fe/src/Component/Footer.jsx
+++ fe/src/Component/Footer.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
 import "./Footer.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMobileAlt, faEnvelope, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faInstagram, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faMobileAlt,
+  faEnvelope,
+  faBars,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   const [email, setEmail] = useState("");
@@ -20,11 +29,21 @@ const Footer = () => {
         <div className="footer-links">
           <h3>Quick Links</h3>
           <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About Us</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">Contact</a></li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Services</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
           </ul>
         </div>
 
@@ -32,12 +51,12 @@ const Footer = () => {
         <div className="footer-subscribe">
           <h3>Subscribe to Our Newsletter</h3>
           <form onSubmit={handleSubscribe}>
-            <input 
-              type="email" 
-              placeholder="Enter your email" 
+            <input
+              type="email"
+              placeholder="Enter your email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              required 
+              required
             />
             <button type="submit">Subscribe</button>
           </form>
@@ -47,19 +66,21 @@ const Footer = () => {
         <div className="footer-contact">
           <h3>Contact Us</h3>
           <p>📍 MJT Building, First Floor 9-A, </p>
-            <p>Bharathiyar Road, P.N. Palayam Coimbatore-641 037</p>
+          <p>Bharathiyar Road, P.N. Palayam Coimbatore-641 037</p>
           <p>📞 +91 89399 41406</p>
           <p>📧 naveen.sankar@rudronilfinserv.com</p>
-          <p>Follow us :</p>
-                    <p>
-                      <FontAwesomeIcon icon={faFacebook} /> Facebook
-                    </p>
-                    <p>
-                      <FontAwesomeIcon icon={faInstagram} /> Instagram
-                    </p>
-                    <p>
-                      <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
-                    </p>
+          <div className="social-footer">
+            <p>Follow us :</p>
+            <p>
+              <FontAwesomeIcon icon={faFacebook} /> Facebook
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faInstagram} /> Instagram
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faLinkedin} /> LinkedIn
+            </p>
+          </div>
         </div>
       </div>
 
